dd2bb9ad17cb871dd64167ad4dacea9d336af401
Lukasz Soszynski <lsoszyn@gmail.com>
2018-05-10 22:06:25 +0200

    amqp seconda ryadapter

--- amqp-secondary-adapter/src/main/java/com/impaqgroup/training/architecture/hexagonalarchitecture/notification/amqpsecondaryadapter/ForumNotificationToAmqpNotificationDtoConverter.java
+++ amqp-secondary-adapter/src/main/java/com/impaqgroup/training/architecture/hexagonalarchitecture/notification/amqpsecondaryadapter/ForumNotificationToAmqpNotificationDtoConverter.java
@@ -1,9 +1,10 @@
-package com.impaqgroup.training.architecture.hexagonalarchitecture.notification.amqp;
+package com.impaqgroup.training.architecture.hexagonalarchitecture.notification.amqpsecondaryadapter;
 
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
 import com.impaqgroup.training.architecture.hexagonalarchitecture.model.notification.ForumNotification;
+import com.impaqgroup.training.architecture.hexagonalarchitecture.notification.amqp.AmqpForumNotificationDto;
 
 @Component
 class ForumNotificationToAmqpNotificationDtoConverter implements Converter<ForumNotification, AmqpForumNotificationDto>{
--- repository/src/main/java/com/impaqgroup/training/architecture/hexagonalarchitecture/repository/jpa/ForumRepository.java
+++ repository/src/main/java/com/impaqgroup/training/architecture/hexagonalarchitecture/repository/jpa/ForumRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.impaqgroup.training.architecture.hexagonalarchitecture.model.Forum;
 
-interface ForumRepository extends JpaRepository<Forum, String> {
+public interface ForumRepository extends JpaRepository<Forum, String> {
 }
